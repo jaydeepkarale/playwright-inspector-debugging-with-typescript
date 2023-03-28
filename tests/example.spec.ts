b0b3bb4e61be19c8502fa3e6a71c4033c885b3b2
Jaydeep Karale <97970643+jaydeepkarale@users.noreply.github.com>
2023-03-28 21:01:47 +0530

    Update example.spec.ts

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -18,3 +18,14 @@ test('forgotten password to be visible', async ({ page }) => {
   await page.pause()
   await expect(page.getByRole('link', { name: 'Forgotten Password', exact: true })).toBeVisible()
 });
+
+test('dynamic element', async ({ page }) => {
+  await page.goto('https://ecommerce-playground.lambdatest.io/index.php?route=product/category&path=57');
+  await page.mouse.wheel(0, 50)
+  await page.getByRole('link', { name: 'HTC Touch HD HTC Touch HD HTC Touch HD HTC Touch HD' }).hover()
+  await page.locator('button:nth-child(3)').first().click();
+  await page.getByRole('button', { name: 'Increase quantity' }).click({clickCount: 5});
+  await expect(page.getByRole("spinbutton", { name :'Qty' })).toHaveValue('6')
+  await expect(page.locator('#image-gallery-212946').getByRole('link', { name: 'HTC Touch HD' })).toHaveCount(5)
+  await page.pause()
+});
